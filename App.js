9d7273dae58f385487462f3b437f39b49e28d89c
Pradeep <pradeepsen99@gmail.com>
2018-01-03 03:17:44 -0600

    added geolocation

--- App.js
+++ App.js
@@ -6,14 +6,37 @@ export default class App extends Component {
         super(props);
         this.state = {
             isLoading: true,
-            dataSource: null
+            dataSource: null,
+            latitude: null,
+            longitude: null
         }
+    }
+
+
+    /**
+     * This function gets the current geo location of the device and stores it
+     * as Lat and Long coordinates.
+     * @private
+     */
+    _getLocation(){
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                    error: null,
+                });
+            },
+            (error) => this.setState({ error: error.message }),
+            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+        );
     }
 
     componentDidMount() {
         return fetch('https://developer.cumtd.com/api/v2.2/JSON/getstops?key=a2142759b9ac473e8dbdb95572546a7b')
             .then((response) => response.json())
             .then((responseJson) => {
+                this._getLocation();
                 let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
                 this.setState({
                     isLoading: false,
@@ -28,16 +51,22 @@ export default class App extends Component {
     }
 
     render() {
+        if (this.state.isLoading) {
+            return (
+                <View style={{flex: 1, paddingTop: 20}}>
+                    <ActivityIndicator />
+                </View>
+            );
+        }
         return (
             <View style={{flex: 1, paddingTop: 20}}>
+                <Text>Latitude: {this.state.latitude}</Text>
+                <Text>Longitude: {this.state.longitude}</Text>
                 <Picker
                     selectedValue={this.state.dataSource}>
                     {this.state.dataSource.map((item, key)=>(
-                        <Picker.Item label={item.stop_name} value={item.stop_name} key={key} />)
-                    )}
+                        <Picker.Item label={item.stop_name} value={item.stop_name} key={key}/>))}
                 </Picker>
-
-
             </View>
         );
     }
